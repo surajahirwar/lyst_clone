d51cba324fb579ae1f980fb6777bf2047218deb6
Suraj Ahirwar <75873682+surajahirwar@users.noreply.github.com>
2022-05-06 11:20:30 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-// import { useState } from 'react'
-import "./App.css";
-// import { Notes } from "./components/fetchdata";
-import { Footer } from "./components/footer/footer";
-import { Header } from "./components/header/header";
-import ProductDetail from "./components/ProductDetail/ProductDetail";
+import './App.css'
+import { Footer } from './components/footer/footer'
+import { Header } from './components/header/header'
+
 
 function App() {
+
+
+
   return (
     <div className="App">
       <Header />
-      <ProductDetail />
-      {/* <Notes /> */}
-
       <h1>hello there</h1>
-
+     
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
