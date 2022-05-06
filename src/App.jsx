89b440253c18a40f4492a66158d74bde105f6470
Poovendharan Selvaraj <97455003+PoovendharanSelvaraj@users.noreply.github.com>
2022-05-06 18:15:05 +0530

    productpage

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
+        
         <Route path="/" element={<HomePage />} />
         <Route path="/product" element={<ProductPage />} />
         <Route path="/product/:id" element={<ProductDetail />} />
