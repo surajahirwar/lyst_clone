62eca9d44ea2c48685e3a238e4795c27ca7fcce2
Soumya <a2soumya.ranjan@gmail.com>
2022-05-07 15:21:26 +0530

    Login and Register

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import { Footer } from "./components/footer/footer";
 import { Header } from "./components/header/header";
 import ProductDetail from "./components/ProductDetail/ProductDetail";
 import { Routes, Route } from "react-router-dom";
-import Home from "./components/HomePage/HomePage";
+import {Home} from "./components/HomePage/HomePage";
 import ProductPage from "./components/ProductPage/ProductPage";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 import CartPage from "./components/CartPage/CartPage";
 import { Paymentdetails } from "./components/PaymentPage/paymentDetails";
+
 
 function App() {
 
@@ -23,6 +25,7 @@ function App() {
         <Route path="/product" element={<ProductPage />} />
         <Route path="/product/:id" element={<ProductDetail />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>}/>
         <Route path="/payment" element={<Paymentdetails />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
